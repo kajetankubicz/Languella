fa774adf68cfbbcdb436a22bc3dfc07f9ea46699
kajetankubicz <kajetan.kubicz12@gmail.com>
2022-11-05 16:04:37 +0100

    Added GameLoop

--- app/src/main/java/com/example/languella/MainActivity.java
+++ app/src/main/java/com/example/languella/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.languella;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Sets window to fullscreen
+        Window windows = getWindow();
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        setContentView(new Game(this));
     }
 }
